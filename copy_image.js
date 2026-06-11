6f500cf1c48d2ed7b1801e27b5311930184f3e8f
Akshay Aher <akshay.aher@netcorecloud.com>
2026-06-11 18:48:31 +0530

    BgImage added.

---
+++ copy_image.js
@@ -0,0 +1,5 @@
+const fs = require('fs');
+fs.copyFileSync(
+  'C:\\Users\\user\\.gemini\\antigravity\\brain\\ad5c9624-d3d5-4e07-ad68-347c648ca679\\auth_bg_image_1781180161097.png',
+  'c:\\Angular\\staff-leave-management\\src\\assets\\auth_bg_image.png'
+);
