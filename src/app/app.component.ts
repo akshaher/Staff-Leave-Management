c786898d6e1e9070cc5ca634e2eed23907dcf2f0
Akshay Aher <akshayaher9696@gmail.com>
2026-08-01 16:40:30 +0530

    Bootstrap the application using the AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnChanges, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    standalone: true,
+    imports: [RouterOutlet]
 })
 export class AppComponent implements OnChanges, OnInit {
  protected   title = 'staff-leave-management';
